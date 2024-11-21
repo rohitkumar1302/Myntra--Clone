80be0bda102e69b98c833d5353a0ded135d64ade
Rohit Kumar <rk8106263@gmail.com>
2024-11-21 16:49:33 +0530

    Project done

---
+++ 3-myntra-react-clone/src/main.jsx
@@ -0,0 +1,32 @@
+import { createRoot } from "react-dom/client";
+import React from "react";
+import App from "./routes/App.jsx";
+import "./index.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./routes/Home.jsx";
+import Bag from "./routes/Bag.jsx";
+import { Provider } from "react-redux";
+import myntraStore from "./store/index.js";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Provider store={myntraStore}>
+      <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
